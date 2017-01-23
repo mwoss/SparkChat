c879a9082de0280662b2e4dc50b78699cf3728bb
mwoss <wos.mateusz16@gmail.com>
2017-01-24 00:13:53 +0100

    cookies handler

--- src/main/java/Cookies.java
+++ src/main/java/Cookies.java
@@ -5,13 +5,18 @@ import java.util.List;
  * Created by Matthew on 2017-01-22.
  */
 public class Cookies {
-    String getName(List<HttpCookie> cookies, String name){
-        return null;
+
+    String iterOverCookiesAndFind(List<HttpCookie> cookies, String name){
+        for(HttpCookie iter : cookies){
+            if(iter.getName().equals(name))
+                return iter.getValue();
+        }
+        return "";
     }
     String getNickName(List<HttpCookie> cookies){
-        return null;
+        return this.iterOverCookiesAndFind(cookies,"username");
     }
     String getChannelName(List<HttpCookie> cookies){
-        return null;
+        return this.iterOverCookiesAndFind(cookies,"channelName");
     }
 }
