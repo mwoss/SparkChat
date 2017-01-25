8a05989fdcb4d933f875966b7a7881fbd461e6c2
mwoss <wos.mateusz16@gmail.com>
2017-01-25 02:42:11 +0100

    refactor name, gson lib

--- src/main/java/ChatInit.java
+++ src/main/java/ChatInit.java
@@ -3,7 +3,7 @@ import static spark.Spark.*;
 /**
  * Created by Matthew on 2017-01-13.
  */
-public class Main { // przenisc do klasy chat
+public class ChatInit { // przenisc do klasy chat
 
     public static void main(String[] args) {
         staticFileLocation("/public"); //index.html is served at localhost:4567 (default port)
